0ab143c91ad3c6d6531fe37b8d8bb61b51a840a3
L0rd5had0w <xtremeshadow@gmail.com>
2022-09-08 22:00:06 -0600

    Credenciales de Paypal en el archivo env.

--- config/services.php
+++ config/services.php
@@ -32,7 +32,7 @@ return [
 
     'paypal' => [
         'client_id' => env('PAYPAL_CLIENT_ID'),
-        'secret' => env('PAYPAL_SECRET')
+        'secret' => env('PAYPAL_CLIENT_SECRET')
     ],
 
 ];
