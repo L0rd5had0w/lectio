61b610c2b6d7e0cd0a5f520760a7465d682a66b2
L0rd5had0w <xtremeshadow@gmail.com>
2023-12-13 18:55:10 -0600

    Depuración de imagenes y parametros.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -132,78 +132,7 @@
             <div class="flex flex-wrap">
                 <div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
                     <div class="px-6">
-                        <img alt="..." src="{{ asset('img/team1.png') }}"
-                            class="shadow-lg rounded-full max-w-full mx-auto" style="max-width: 120px" />
-                        <div class="pt-6 text-center">
-                            <h5 class="text-xl font-bold">Victor Blanco</h5>
-                            <p class="mt-1 text-sm text-gray-500 uppercase font-semibold">
-                                Diseñádor
-                            </p>
-                            <div class="mt-6">
-                                <button
-                                    class="bg-blue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-                                    type="button">
-                                    <i class="fab fa-twitter"></i></button><button
-                                    class="bg-blue-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-                                    type="button">
-                                    <i class="fab fa-facebook-f"></i></button><button
-                                    class="bg-pink-500 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-                                    type="button">
-                                    <i class="fab fa-dribbble"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
-                    <div class="px-6">
-                        <img alt="..." src="{{ asset('img/team1.png') }}"
-                            class="shadow-lg rounded-full max-w-full mx-auto" style="max-width: 120px" />
-                        <div class="pt-6 text-center">
-                            <h5 class="text-xl font-bold">Victor Blanco</h5>
-                            <p class="mt-1 text-sm text-gray-500 uppercase font-semibold">
-                                Arquitecto
-                            </p>
-                            <div class="mt-6">
-                                <button
-                                    class="bg-red-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-                                    type="button">
-                                    <i class="fab fa-google"></i></button><button
-                                    class="bg-blue-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-                                    type="button">
-                                    <i class="fab fa-facebook-f"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
-                    <div class="px-6">
-                        <img alt="..." src="{{ asset('img/team1.png') }}"
-                            class="shadow-lg rounded-full max-w-full mx-auto" style="max-width: 120px" />
-                        <div class="pt-6 text-center">
-                            <h5 class="text-xl font-bold">Victor Blanco</h5>
-                            <p class="mt-1 text-sm text-gray-500 uppercase font-semibold">
-                                Progrador </p>
-                            <div class="mt-6">
-                                <button
-                                    class="bg-red-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-                                    type="button">
-                                    <i class="fab fa-google"></i></button><button
-                                    class="bg-blue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-                                    type="button">
-                                    <i class="fab fa-twitter"></i></button><button
-                                    class="bg-gray-800 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-                                    type="button">
-                                    <i class="fab fa-instagram"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
-                    <div class="px-6">
-                        <img alt="..." src="{{ asset('img/team1.png') }}"
+                        <img alt="..." src="{{ asset('img/vblanco.png') }}"
                             class="shadow-lg rounded-full max-w-full mx-auto" style="max-width: 120px" />
                         <div class="pt-6 text-center">
                             <h5 class="text-xl font-bold">Victor Blanco</h5>
